9c2a25fcdd365eb8375076a09276e31402fc846c
Victor Hugo da Silva Dias <victorhugo.dias@uol.com.br>
2018-05-25 19:06:57 -0300

    Create function to generate image list

--- detect.py
+++ detect.py
@@ -1,8 +1,77 @@
 import argparse
+import hashlib
+import re
+import collections
 import os.path
 import sys
 
 import tensorflow as tf
+
+
+def create_image_lists(image_dir, max_images, validation_percentage, testing_percentage):
+    if not tf.gfile.Exists(image_dir):
+        tf.logging.error("Image directory '" + image_dir + "' not found.")
+        return None
+    result = collections.OrderedDict()
+    sub_dirs = sorted(x[0] for x in tf.gfile.Walk(image_dir))
+    # The root directory comes first, so skip it.
+    is_root_dir = True
+    for sub_dir in sub_dirs:
+        if is_root_dir:
+            is_root_dir = False
+            continue
+        extensions = ['jpg', 'jpeg']
+        file_list = []
+        dir_name = os.path.basename(sub_dir)
+        if dir_name == image_dir:
+            continue
+        tf.logging.info("Looking for images in '" + dir_name + "'")
+        for extension in extensions:
+            file_glob = os.path.join(image_dir, dir_name, '*.' + extension)
+            file_list.extend(tf.gfile.Glob(file_glob))
+        if not file_list:
+            tf.logging.warning('No files found')
+            continue
+        if len(file_list) > max_images:
+            tf.logging.warning(
+                'WARNING: Folder {} have {} images more than the limit of {} images. Some images will '
+                'never be selected.'.format(dir_name, len(file_list),max_images))
+        label_name = re.sub(r'[^a-z0-9]+', ' ', dir_name.lower())
+        training_images = []
+        testing_images = []
+        validation_images = []
+        for file_name in file_list:
+            base_name = os.path.basename(file_name)
+            # We want to ignore anything after '_nohash_' in the file name when
+            # deciding which set to put an image in, the data set creator has a way of
+            # grouping photos that are close variations of each other. For example
+            # this is used in the plant disease data set to group multiple pictures of
+            # the same leaf.
+            hash_name = re.sub(r'_nohash_.*$', '', file_name)
+            # This looks a bit magical, but we need to decide whether this file should
+            # go into the training, testing, or validation sets, and we want to keep
+            # existing files in the same set even if more files are subsequently
+            # added.
+            # To do that, we need a stable way of deciding based on just the file name
+            # itself, so we do a hash of that and then use that to generate a
+            # probability value that we use to assign it.
+            hash_name_hashed = hashlib.sha1(tf.compat.as_bytes(hash_name)).hexdigest()
+            percentage_hash = ((int(hash_name_hashed, 16) %
+                                (max_images + 1)) *
+                               (100.0 / max_images))
+            if percentage_hash < validation_percentage:
+                validation_images.append(base_name)
+            elif percentage_hash < (testing_percentage + validation_percentage):
+                testing_images.append(base_name)
+            else:
+                training_images.append(base_name)
+        result[label_name] = {
+            'dir': dir_name,
+            'training': training_images,
+            'testing': testing_images,
+            'validation': validation_images,
+        }
+    return result
 
 
 def ensure_dir_exists(dir_name):
@@ -36,6 +105,9 @@ def main(_):
     # Prepare necessary directories that can be used during training
     prepare_file_system()
 
+    # Create lists of all the images.
+    image_lists = create_image_lists(FLAGS.image_dir, FLAGS.max_images_per_class, FLAGS.testing_percentage,
+                                     FLAGS.validation_percentage)
 
 
 if __name__ == '__main__':
@@ -72,6 +144,24 @@ if __name__ == '__main__':
              How many steps to store intermediate graph. If "0" then will not
              store.\
           """
+    )
+    parser.add_argument(
+        '--max_images_per_class',
+        type=int,
+        default=500,
+        help='How many images will be selected'
+    )
+    parser.add_argument(
+        '--testing_percentage',
+        type=int,
+        default=10,
+        help='What percentage of images to use as a test set.'
+    )
+    parser.add_argument(
+        '--validation_percentage',
+        type=int,
+        default=10,
+        help='What percentage of images to use as a validation set.'
     )
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
