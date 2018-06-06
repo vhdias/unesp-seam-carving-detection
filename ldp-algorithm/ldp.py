8d0bec7326f218af35fcbc857beb82e9927235f0
João Pedro <jotamucheroni@gmail.com>
2018-06-06 11:44:52 -0300

    LDP class refactoring

--- ldp-algorithm/ldp.py
+++ ldp-algorithm/ldp.py
@@ -3,79 +3,73 @@
 import cv2
 import numpy as np
 
-class DirectionalDerivative:
-	def __init__(self, bounds, np_type):
-		try:
-			# Directional derivatives with radius 1 e 2
-			self.dir0  = { 1 : np.zeros(bounds, np_type), 2 : np.zeros(bounds, np_type) }
-			self.dir90 = { 1 : np.zeros(bounds, np_type), 2 : np.zeros(bounds, np_type) }
-		except:
-			print("Bounds must be a tuple of int and np_type a numpy type")
 
 class LDP:
+	ORDER_INDEXES = (1, 2)
+	RADIUS_INDEXES = (1, 2)
+	ANGLE_INDEXES = (0, 90)
+	ANGLE_DIS = {0: (0, 1), 90: (-1, 0)}
+
 	def set_image(self, image):
 		self.image = image
-		
+
 		try:
 			# Shape = image dimensions
 			self.height, self.width, channel = image.shape
 			# Convert image to grayscale
 			self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-		except:
+		except ValueError:
 			print("Image must be class 'numpy.ndarray'")
-	
+
 	def __init__(self, image):
-		set_image(self, image)
-	
+		self.image = None
+		self.height, self.width = 0, 0
+		self.set_image(image)
+		self.derivative, self.ldp = {}, {}
+		bounds = (self.height, self.width)
+
+		# For ldp, 1 and 2 mean second and third order respectively
+		for order in (1, 2):
+			for angle in (0, 90):
+				for radius in (1, 2):
+					self.derivative[order, angle, radius] = np.zeros(bounds, np.int16)
+					self.ldp[order, angle, radius] = np.zeros(bounds, np.uint8)
+
 	# Bilinear interpolation
-	def bi_inter(x, y):
+	def bi_inter(self, x, y):
 		pass
-		
+
+	@staticmethod
 	def calculate_bin(z0, zi):
 		return 0 if z0 * zi > 0 else 1
-		
-	def calculate_byte(ang, order, radius, i, j):
-		matrix, _dir = self.derivative[order - 1], self.ldp[order]
-		matrix, _dir = (matrix.dir0, _dir.dir0) if ang == 0 else (matrix.dir90, _dir.dir90)
-		matrix, _dir = matrix[radius], _dir[radius][i, j]
-		
+
+	def calculate_byte(self, ang, order, radius, i, j):
+		matrix, _dir = self.derivative[order, ang, radius], self.ldp[order, ang, radius][i, j]
+
 		for dis_lin, dis_col in [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]:
-			_dir << 1
-			_dir += calculate_bin( matrix[i, j], matrix[i + dis_lin, j + dis_col] )
-		
+			_dir <<= 1
+			_dir += LDP.calculate_bin(matrix[i, j], matrix[i + dis_lin, j + dis_col])
+
 	def calculate_features(self):
-		bounds = (height, width)
-		
-		# First and second derivative of each pixel
-		self.derivative = { 1 : DirectionalDerivative( bounds, np.int16), 2 :  DirectionalDerivative( bounds, np.int16) }
-		# LDP
-		self.ldp = { 2 : DirectionalDerivative( bounds, np.uint8), 3 : DirectionalDerivative( bounds, np.uint8)}
-		
-		limits = []
-		matrix =  []
-		
 		# Calculate derivatives
-		for order in range(1, 3):
-			for radius in range(1, 3):
-				limits = [ (1 + order) * radius, radius]
-				matrix =  [self.image, self.image] if order == 1 else [self.derivative[1].dir0, self.derivative[1].dir90]
-				
-				for i in range(limits[0], height - limits[1]):
-					for j in range(limits[1], width - limits[0]):
-						self.derivative[order].dir0[radius][i, j]  = matrix[0][i, j] - matrix[0][i, j + radius]
-						self.derivative[order].dir90[radius][i, j] = matrix[1][i, j] - matrix[1][i - radius, j]
-		
-		
-		
+		for order in LDP.ORDER_INDEXES:
+			for angle in LDP.ANGLE_INDEXES:
+				for radius in LDP.RADIUS_INDEXES:
+					limits = [(1 + order) * radius, radius]
+					matrix = self.image if order == 1 else self.derivative[order - 1, angle, radius]
+
+					for i in range(limits[0], self.height - limits[1]):
+						for j in range(limits[1], self.width - limits[0]):
+							self.derivative[order, angle, radius][i, j] = matrix[i, j] - matrix[
+								i + LDP.ANGLE_DIS[angle][0] * radius,
+								j + LDP.ANGLE_DIS[angle][1] * radius]
+
 		# Calculate LDPs
-		for ldp_order in range(2, 4):
-			for radius in range(1, 3):
-				limits = [ ldp_order * radius, radius]
-				
-				for i in range(limits[0], height - limits[1]):
-					for j in range(limits[1], width - limits[0]):
-						calculate_byte(0, ldp_order, radius, i, j)
-						calculate_byte(90, ldp_order, radius, i, j)
-											
-						
-						
+		for order in LDP.ORDER_INDEXES:
+			for angle in LDP.ANGLE_INDEXES:
+				for radius in LDP.RADIUS_INDEXES:
+					limits = [(1 + order) * radius, radius]
+
+					for i in range(limits[0], self.height - limits[1]):
+						for j in range(limits[1], self.width - limits[0]):
+							self.calculate_byte(order, angle, radius, i, j)
