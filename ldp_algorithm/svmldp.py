1e45e953011fd824efe39ea73285504990813bdf
João Pedro <jotamucheroni@gmail.com>
2018-06-15 17:19:46 -0300

    SVMLDP version 1
    SVMLDP using list of image paths

--- ldp_algorithm/svmldp.py
+++ ldp_algorithm/svmldp.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 class SVMLDP:
-	label_column_list = []
+	label_feature_list = []
 	column_dict = {}
 	class_list = []
 
@@ -25,7 +25,7 @@ class SVMLDP:
 		self.set_not_edited_list(not_edited_path_list)
 		self.num_train_images = len(self.edited_path_list) + len(self.not_edited_path_list)
 
-		self.svm = tf.contrib.learn.SVM(feature_columns=SVMLDP.label_column_list, example_id_column='example_id')
+		self.svm = tf.contrib.learn.SVM(feature_columns=SVMLDP.label_feature_list, example_id_column='example_id')
 
 	@staticmethod
 	def input_fn():
@@ -35,12 +35,35 @@ class SVMLDP:
 	def predict_fn():
 		return SVMLDP.column_dict
 
+	def fill_features(self, image_paths):
+		for column, image_path in list(enumerate(image_paths)):
+			image = LDP(image_path)
+			image.calculate_features()
+
+			line = 0
+			for order in LDP.ORDER_INDEXES:
+				for angle in LDP.ANGLE_INDEXES:
+					for radius in LDP.RADIUS_INDEXES:
+						for byte in range(256):
+							self.column_dict[SVMLDP.label_feature_list[line][0]][column] = image.histograms[order, angle, radius][byte]
+							line += 1
+
+		for line in range(len(SVMLDP.label_feature_list)):
+			self.column_dict[SVMLDP.label_feature_list[line][0]] = tf.constant(
+				self.column_dict[SVMLDP.label_feature_list[line][0]])
+
 	def fit(self):
 		self.column_dict = {'example_id': tf.constant([str(k) for k in range(1, self.num_train_images + 1)])}
 		self.class_list = np.zeros(self.num_train_images, np.uint8)
 		self.class_list[:len(self.edited_path_list)] = 1
-		# TODO
-		# Generate columns
+
+		for feature in SVMLDP.label_feature_list:
+			self.column_dict[feature[0]] = np.zeros(self.num_train_images, np.float32)
+
+		image_paths = self.edited_path_list + self.not_edited_path_list
+
+		self.fill_features(image_paths)
+
 		SVMLDP.column_dict = self.column_dict
 		SVMLDP.class_list = self.class_list
 		self.svm.fit(input_fn=SVMLDP.input_fn)
@@ -53,13 +76,16 @@ class SVMLDP:
 
 	def predict(self, path_list):
 		del self.column_dict['example_id']
-		# TODO
-		# Generate columns
+		self.fill_features(path_list)
+		SVMLDP.column_dict = self.column_dict
 		self.results = self.svm.predict(input_fn=SVMLDP.predict_fn)
 		return self.results
 
 
-for order in LDP.ORDER_INDEXES:
-	for angle in LDP.ANGLE_INDEXES:
-		for radius in LDP.RADIUS_INDEXES:
-			SVMLDP.label_column_list.append(tf.contrib.layers.real_valued_column('(%d, %d, %d)' % (order, angle, radius)))
+for _order in LDP.ORDER_INDEXES:
+	for _angle in LDP.ANGLE_INDEXES:
+		for _radius in LDP.RADIUS_INDEXES:
+			for _byte in range(256):
+				SVMLDP.label_feature_list.append(tf.contrib.layers.real_valued_column('(%d, %d, %d, %d)' %
+				                                                                      (_order, _angle, _radius, _byte)))
+
