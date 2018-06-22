a9a92b46f9a0a69db1f2c24db2f7962448bdc9e4
João Pedro <jotamucheroni@gmail.com>
2018-06-22 02:50:18 -0300

    Corrections SVM #2
    TensorFlow constants createds inside input and predict function

--- svm.py
+++ svm.py
@@ -29,48 +29,51 @@ class SVM:
 		self.set_not_edited_data_frame(not_edited_csv_path)
 
 		if self.edited_data_frame.shape[0] != self.not_edited_data_frame.shape[0]:
-			raise ValueError("The number of features must be equal in edited e not edited images")
+			raise ValueError('The number of features must be equal in edited e not edited images')
 
 		self.set_list_dict(list_dict)
 
 		for line in range(1, self.edited_data_frame.shape[0] + 1):
-			SVM.label_feature_list.append(tf.contrib.layers.real_valued_column("f%d" % line))
+			SVM.label_feature_list.append(tf.contrib.layers.real_valued_column('f%d' % line))
 
 		self.svm = tf.contrib.learn.SVM(feature_columns=SVM.label_feature_list, example_id_column='example_id',
-		                                model_dir="./output")
+		                                model_dir='./output')
 
 	@staticmethod
 	def input_fn():
-		return SVM.column_dict, SVM.class_list
+		_column_dict = {'example_id': tf.constant(SVM.column_dict['example_id'])}
+		for line in range(len(SVM.label_feature_list)):
+			_column_dict[SVM.label_feature_list[line][0]] = tf.constant(
+				SVM.column_dict[SVM.label_feature_list[line][0]])
+		return _column_dict, tf.constant(SVM.class_list)
 
 	@staticmethod
 	def predict_fn():
-		return SVM.column_dict
+		_column_dict = {}
+		for line in range(len(SVM.label_feature_list)):
+			_column_dict[SVM.label_feature_list[line][0]] = tf.constant(
+				SVM.column_dict[SVM.label_feature_list[line][0]])
+		return _column_dict
 
 	def fill_features(self, edited_image_names, not_edited_image_names):
 		col = 0
 		for image in edited_image_names:
-			values = self.edited_data_frame[image].values
+			values = self.edited_data_frame[image].values.astype(np.float32)
 			for line in range(self.edited_data_frame.shape[0]):
 				self.column_dict[SVM.label_feature_list[line][0]][col] = values[line]
 			col += 1
 
 		for image in not_edited_image_names:
-			values = self.not_edited_data_frame[image].values
+			values = self.not_edited_data_frame[image].values.astype(np.float32)
 			for line in range(self.not_edited_data_frame.shape[0]):
 				self.column_dict[SVM.label_feature_list[line][0]][col] = values[line]
 			col += 1
-
-		for line in range(len(SVM.label_feature_list)):
-			self.column_dict[SVM.label_feature_list[line][0]] = tf.constant(
-				self.column_dict[SVM.label_feature_list[line][0]])
 
 	def create_dictionary(self, edited_images, not_edited_images):
 		images = edited_images + not_edited_images
 		num_images = len(images)
 		self.class_list = np.zeros(num_images, np.uint8)
 		self.class_list[:len(edited_images)] = 1
-		self.class_list = tf.constant(self.class_list)
 
 		for feature in SVM.label_feature_list:
 			self.column_dict[feature[0]] = np.zeros(num_images, np.float32)
@@ -78,17 +81,15 @@ class SVM:
 		self.fill_features(edited_images, not_edited_images)
 
 	def fit(self):
-		self.column_dict = {'example_id': tf.constant(self.list_dict['seam carved']['training'] +
-		                                              self.list_dict['untouched']['training'])}
+		self.column_dict = {'example_id': self.list_dict['seam carved']['training'] + self.list_dict['untouched']['training']}
 		self.create_dictionary(self.list_dict['seam carved']['training'], self.list_dict['untouched']['training'])
 
 		SVM.column_dict = self.column_dict
 		SVM.class_list = self.class_list
-		self.svm.fit(input_fn=SVM.input_fn)
+		self.svm.fit(input_fn=SVM.input_fn, steps=30)
 
 	def evaluate(self):
-		self.column_dict = {'example_id': tf.constant(self.list_dict['seam carved']['validation'] +
-		                                              self.list_dict['untouched']['validation'])}
+		self.column_dict = {'example_id': self.list_dict['seam carved']['validation'] + self.list_dict['untouched']['validation']}
 		self.create_dictionary(self.list_dict['seam carved']['validation'], self.list_dict['untouched']['validation'])
 
 		SVM.column_dict = self.column_dict
@@ -103,3 +104,14 @@ class SVM:
 		SVM.column_dict = self.column_dict
 		self.results = self.svm.predict(input_fn=SVM.predict_fn)
 		return self.results
+
+
+from lists import ImageList
+
+directory = '/home/jota/Downloads/'
+dic = ImageList(directory + 'seam_images', 500, 10, 10)
+svm = SVM(directory + 'sc.csv', directory + 'nao.csv', dic.list)
+svm.fit()
+metrics = svm.evaluate()
+result = svm.predict()
+input('Sair')
